d10843fb58e9792cdd49679a572f7ac1d640f936
shotout <hevaldo66@gmail.com>
2022-11-21 15:43:55 +0700

    Update Subscription Purchasel

---
+++ app/Http/Controllers/Api/v1/SubscriptionsController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Subscription;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class SubscriptionsController extends Controller
+{
+    public function index()
+    {
+        $subscriptions = Subscription::where('user_id', auth()->user()->id)->get();
+        return response()->json($subscriptions);
+    }
+
+    public function subscribe(Request $request)
+    {
+        $request->validate([
+            'subscription_type' => 'required',
+        ]);
+
+        if ($request->subscription_type == 1 ) {
+
+            $subscriptions = Subscription::where('user_id', auth()->user()->id)->first();
+
+
+            if ($subscriptions) {
+                $subscriptions->type = $request->subscription_type;
+                $subscriptions->plan_id = 1;
+                $subscriptions->started = null;
+                $subscriptions->renewal = null;
+                $subscriptions->save();
+            } else {
+                $subscriptions = new Subscription;
+                $subscriptions->user_id = auth()->user()->id;
+                $subscriptions->type = $request->subscription_type;
+                $subscriptions->plan_id = 1;
+                $subscriptions->started = null;
+                $subscriptions->renewal = null;
+                $subscriptions->save();
+            }
+
+            return response()->json([
+                'status' => 'ok',
+                'data' => $subscriptions
+            ], 200);
+        }
+
+        if ($request->subscription_type == 2) {
+
+            $subscriptions = Subscription::where('user_id', auth()->user()->id)->first();
+            if ($subscriptions) {
+                $subscriptions->type = $request->subscription_type;
+                $subscriptions->plan_id = 2;
+                $subscriptions->started = Carbon::now();
+                $subscriptions->renewal = Carbon::now()->addDay(2);
+                $subscriptions->save();
+            } else {
+                $subscriptions = new Subscription;
+                $subscriptions->user_id = auth()->user()->id;
+                $subscriptions->type = $request->subscription_type;
+                $subscriptions->plan_id = 2;
+                $subscriptions->started = Carbon::now();
+                $subscriptions->renewal = Carbon::now()->addDay(2);
+                $subscriptions->save();
+            }
+
+            return response()->json([
+                'status' => 'ok',
+                'data' => $subscriptions
+            ], 200);
+        }
+
+        if ($request->subscription_type == 3) {
+
+            $subscriptions = Subscription::where('user_id', auth()->user()->id)->first();
+            if ($subscriptions) {
+                $subscriptions->type = $request->subscription_type;
+                $subscriptions->plan_id = 3;
+                $subscriptions->started = Carbon::now();
+                $subscriptions->renewal = Carbon::now()->addMonth(1);
+                $subscriptions->save();             
+
+            } else {
+                $subscriptions = new Subscription;
+                $subscriptions->user_id = auth()->user()->id;
+                $subscriptions->type = $request->subscription_type;
+                $subscriptions->plan_id = 3;
+                $subscriptions->started = Carbon::now();
+                $subscriptions->renewal = Carbon::now()->addMonth(1);
+                $subscriptions->save();
+            }
+            
+            return response()->json([
+                'status' => 'ok',
+                'data' => $subscriptions
+            ], 200);
+
+        }        
+
+      
+    }
+}
