5ddf75df77311d4989a6fa669c12865f3acc43e5
shotout <hevaldo66@gmail.com>
2022-11-28 10:35:20 +0700

    add purchasely webhook

--- app/Http/Controllers/Api/v1/PurchaselyController.php
+++ app/Http/Controllers/Api/v1/PurchaselyController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Log;
 
 class PurchaselyController extends Controller
 {
-    public function index(request $request)
+    public function webhook(request $request)
     {
         $data = $request->all();
         Log::info($data);
