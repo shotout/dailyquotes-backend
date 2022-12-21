c14a84ffe0ba449ce6ec24a387c71067fd4b4afa
shotout <hevaldo66@gmail.com>
2022-12-21 11:00:06 +0700

    update import method

--- app/Imports/QuotesImport.php
+++ app/Imports/QuotesImport.php
@@ -3,12 +3,14 @@
 namespace App\Imports;
 
 use App\Models\Quote;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithBatchInserts;
 use Maatwebsite\Excel\Concerns\WithLimit;
 use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class QuotesImport implements ToModel,WithStartRow,WithBatchInserts
+class QuotesImport implements ToCollection,WithStartRow,WithBatchInserts
 {
 
     public function  __construct($category)
@@ -37,11 +39,16 @@ class QuotesImport implements ToModel,WithStartRow,WithBatchInserts
      *
      * @return \Illuminate\Database\Eloquent\Model|null
      */
-    public function model(array $row)
+    
+    public function collection(Collection $collection)
     {
-        return new Quote([
-            'title'     => $row[0],
-            'category_id'    => $this->category
-        ]);
+        foreach ($collection as $row) 
+        {
+            Quote::create([
+                'title'     => $row[0],
+                'category_id'    => $this->category
+            ]);
+        }
     }
+    
 }
