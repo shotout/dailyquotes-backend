8ca095922e7158618594238498405d2b6d556b5c
Julian <padliyulian@gmail.com>
2023-01-03 13:54:38 +0700

    adding font size on themes

---
+++ database/migrations/2023_01_03_064428_add_font_size_to_themes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFontSizeToThemesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('themes', function (Blueprint $table) {
+            $table->tinyInteger('line_height')->default(24)->after('font_family');
+            $table->tinyInteger('font_size')->default(18)->after('font_family');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('themes', function (Blueprint $table) {
+            $table->dropColumn('line_height','font_size');
+        });
+    }
+}
